d4b955c5887f9edb37964809328e6bac03ca4eb0
carOrLa03 <carmenorla@icloud.com>
2022-09-27 12:17:39 +0200

    test calculadora fin

--- calculadora.js
+++ calculadora.js
@@ -1,18 +1,18 @@
-function suma (numero1, numero2){
-    var resultado = numero1 + numero2
-    return `La suma de ${numero1} + ${numero2} es ${resultado}`;
+function suma (numero1, numero2){ 
+    return numero1 + numero2;
 }
 function resta (numero1, numero2){
-    var resultado = numero1 - numero2
-    return `La resta de ${numero1} - ${numero2} es ${resultado}`;
+    return numero1 - numero2;
 }
 function multiplicacion (numero1, numero2){
-    var resultado = numero1 * numero2
-    return `La multiplicación de ${numero1} * ${numero2} es ${resultado}`;
+    return numero1 * numero2;
 }
 function division (numero1, numero2){
-    var resultado = numero1 / numero2
-    return `La división de ${numero1} / ${numero2} es ${resultado}`;
+    if(numero2===0){
+        throw "No se puede dividir por 0";
+    }
+        return numero1 / numero2; 
+
 }
 module.exports = {
     suma,
--- calculadora.test.js
+++ calculadora.test.js
@@ -14,8 +14,27 @@ describe("valid subtractions", () => {
   test("10 - 2 = 8", () => {
     expect(resta(10, 2)).toEqual(8);
   });
-
   test("87 - 523 = -436", () => {
-    expect(resta(87, 523)).toEqual(-436);
+      expect(resta(87, 523)).toEqual(-436);
+    });
+});
+
+
+describe("valid multiplications", ()=>{
+  test("5 * 10 = 50", () => {
+    expect(multiplicacion(5, 10)).toEqual(50);
   });
+});
+
+describe("valid divisions", ()=>{
+  test("5 / 10 = 0.5", () => {
+    expect(division(5, 10)).toEqual(0.5);
+  });
+
+  test("5 / 0 throws", () => {
+    expect(()=>{
+      expect(division(5, 0));
+    }).toThrow();
+  });
+
 });
